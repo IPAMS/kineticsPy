905962a8dd333f66d0d7faef5afc15afbf0cf047
Walter Wissdorf <mail@walterwissdorf.de>
2021-02-11 00:08:55 +0100

    Adds basic set of high level access methods to trajectory

--- kineticsPy/base/trajectory.py
+++ kineticsPy/base/trajectory.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
 
-#__all__ = (
-#	'Trajectory')
+import pandas as pd
+
+__all__ = ["Trajectory"]
+
+
+class TrajectoryIndexer:
+	"""
+	Simple indexer for the trajectory class
+	(similar to the indexer classes used by Pandas)
+	"""
+
+	def __init__(self, parent_trajectory):
+		self._tajectory = parent_trajectory
+
+	def __getitem__(self, arg):
+		if len(arg) == 1:
+			return self._tajectory.data[arg]
+		elif len(arg) == 2:
+			return self._tajectory.data[arg[0]].iloc[arg[1]]
+		else:
+			raise ValueError('Argument is not 1 or 2 dimensional');
 
 
 class Trajectory:
@@ -13,9 +32,43 @@ class Trajectory:
 	The trajectory class is based on numpy and pandas.
 	"""
 
-	def __init__(self):
+	def __init__(self, species_names, times, data, attributes=None):
+		"""
+		Constructs a new kinetic Trajectory
 
-		self.n_timesteps = -1
+		:param species_names: Names of chemical species in the trajectory
+		:type species_names: list[str]
+		:param times: Times of recorded concentration data points in the trajectory
+		:type times: numpy.ndarray with shape ``[n timesteps, 1]``
+		:param data: Concentration time series, essentially a two dimensional matrix of concentration
+		values for every time step and every time
+		:type times: numpy.ndarray with shape ``[numbe of species, number of time steps]``
+		:param attributes: Optional trajectory attributes / meta data describing the trajectory (e.g. temperatures, pressures)
+		:type attributes: dict
+		"""
 
+		self._species_names = species_names
+		self._times = pd.Series(times)
+		self._n_timesteps = len(times)
+		self._data = pd.DataFrame(data=data, columns=species_names, index=times)
+		self._data.index.name = "Time"
+		self._indexer = TrajectoryIndexer(self)
+
+	@property
 	def number_of_timesteps(self):
-		return self.n_timesteps
+		return self._n_timesteps
+
+	@property
+	def species_names(self):
+		return self._species_names
+
+	@property
+	def data(self):
+		return self._data
+
+	@property
+	def loc(self):
+		return self._indexer
+
+	def __len__(self):
+		return self._n_timesteps
--- test/base/test_trajectory.py
+++ test/base/test_trajectory.py
@@ -1,5 +1,7 @@
 import unittest
 import kineticsPy.base.trajectory as tr
+import pandas as pd
+import pandas.testing as pd_test
 
 
 class TestTrajectory(unittest.TestCase):
@@ -8,7 +10,32 @@ class TestTrajectory(unittest.TestCase):
 	def setUpClass(cls):
 		pass
 
-	def test_basic_access(self):
-		tra = tr.Trajectory()
-		self.assertEqual(tra.n_timesteps, -1)
+	def test_basic_creation_and_access(self):
+
+		species_names = ["A", "B", "H2O", "NO2"]
+		times = [0.0, 2.5, 5.0, 7.5, 10.0, 15.0, 20.0]
+		data_as_lists = [
+			[10, 50,   20.0, 0.005],
+			[12, 100,  19.8, 0.004],
+			[14, 200,  19.6, 0.003],
+			[16, 400,  19.8, 0.002],
+			[18, 800,  18.5, 0.001],
+			[20, 1600, 17.9, 0.00001],
+			[22, 3200, 19.9, 0.0000001]
+		]
+		attributes = None
+
+		tra = tr.Trajectory(species_names, times, data_as_lists, attributes)
+		self.assertEqual(tra.number_of_timesteps, 7)
+		self.assertEqual(tra.species_names, species_names)
+
+		# test access methods:
+
+		# high level access with "loc":
+		self.assertEqual(tra.loc['B'].iloc[2], 200)
+		self.assertEqual(tra.loc['B', 2], 200)
+		pd_test.assert_series_equal(
+			tra.loc['A', 2:4],
+			pd.Series([14, 16], name='A', index=pd.Index([5.0, 7.5], name='Time')))
+
 
