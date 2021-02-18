acb348484a33e750317c66710dea72131cf5fb86
--replace-all <wissdorf@uni-wuppertal.de>
2021-02-18 02:58:21 +0100

    Updates simple test of adiabatic istobar cantera simulation

--- test/simulations/test_cantera_simulation.py
+++ test/simulations/test_cantera_simulation.py
@@ -11,9 +11,14 @@ class TestCanteraAnalysis(unittest.TestCase):
 		cls.water_cluster_input = os.path.join(data_base_path, 'WaterCluster_RoomTemp.cti')
 
 	def test_isobar_adiabatic_simulation(self):
+		# perform a simple simulation of the example water cluster system:
 		sim_result = sim.simulate_isobar_adiabatic(
 			self.water_cluster_input,
 			'H2O:2.5e+14, N2:2.54e+17, H3O+:1e+10',
-			10000, 2e-7, 1430)
+			10000, 2e-7, 100000)
 
-		print(sim_result)
+		# species names have to be correct:
+		self.assertEqual(
+			sim_result.species_names,
+			['N2', 'H2O', 'H3O+', 'H3O+(H2O)', 'H3O+(H2O)2', 'H3O+(H2O)3', 'H3O+(H2O)4'])
+
